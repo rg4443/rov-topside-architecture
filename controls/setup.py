a6d13656cde9c0153cd89afa1dc8816c1d727413
Kyle-Eldridge <keldridge07@gmail.com>
2025-11-20 10:12:29 -0800

    Add pygame depencency

--- controls/setup.py
+++ controls/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pygame'],
     zip_safe=True,
     maintainer='UCSC MATE Team',
     maintainer_email='slugbotics@ucsc.edu',
